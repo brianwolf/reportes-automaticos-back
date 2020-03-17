3314bdffc0bb869dc345694bf8fa63cdad72ce78
Brian Lobo <brian.d.lobo@gmail.com>
2020-03-17 20:23:55 -0300

    acorto nombre de tags

--- scripts/docker/ambiente.sh
+++ scripts/docker/ambiente.sh
@@ -11,12 +11,15 @@ export DOCKER_TOKEN=${DOCKER_TOKEN:-64f0e37f-eb5a-4072-8b5b-a1a2b7e7b7cf}
 # PRIVADAS
 ########################################################
 
-export DOCKER_NOMBRE_IMAGEN=reportes-automaticos-back
-export DOCKER_TAG=${CIRCLE_SHA1:-latest}
-export DOCKER_DOCKERFILE=dockerfile
-
 export DOCKER_ARCHIVO_ARGUMENTOS=./config/argumentos.env
 export DOCKER_ARCHIVO_AMBIENTE=./config/ambiente.env
+
+export DOCKER_DOCKERFILE=dockerfile
+
+export CIRCLE_SHA1_SHORT=${CIRCLE_SHA1:0:7}
+
+export DOCKER_NOMBRE_IMAGEN=circleci
+export DOCKER_TAG=${CIRCLE_SHA1_SHORT:-latest}
 
 export DOCKER_NETWORK=docker_red
 
